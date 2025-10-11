1c71df10234166ee9872cac3fffca33fe5d2bcc9
Andrew Krumbach <andrew.krumbach@snhu.edu>
2025-10-11 16:55:20 -0700

    Module 6 completed baseline

--- app.js
+++ app.js
@@ -30,15 +30,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Enable CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
 const tripsController = require("../controllers/trips");
 
 router
     .route("/trips")
-    .get(tripsController.tripsList);
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip); //POST Method Adds Trip
 
 router
     .route('/trips/:tripCode')
-    .get(tripsController.tripsFindByCode);
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
